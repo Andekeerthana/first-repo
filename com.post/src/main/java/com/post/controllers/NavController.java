a7984f454288d0de1ca86724f42a6158e6f6fbf5
keerthana <keerthana@keerthana>
2024-09-12 18:48:20 +0530

    commit

---
+++ com.post/src/main/java/com/post/controllers/NavController.java
@@ -0,0 +1,13 @@
+package com.post.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class NavController {
+@GetMapping("/")
+public String newPost()
+{
+	return "newPost";
+}
+}
